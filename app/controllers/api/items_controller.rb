e13799d3959e0933cc067664696f6a952c05cc21
wolverine.ks <wolverine.ks@gmail.com>
2015-07-07 11:50:37 -0700

    added delete from command line (users,lists)

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -9,7 +9,18 @@ class Api::ItemsController < ApiController
       render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
     end
   end
- 
+  
+  def destroy
+    begin
+      item = Item.find(params[:id])
+      item.destroy
+
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+
   private
 
   def item_params
